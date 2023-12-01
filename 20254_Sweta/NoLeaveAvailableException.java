59062578233ec588dbc1230e8e7da7910634fe7b
Uday <72154285+RUdaykiran@users.noreply.github.com>
2023-11-30 23:45:14 -0500

    Delete 20254_Sweta directory

--- 20254_Sweta/NoLeaveAvailableException.java
+++
@@ -1,9 +0,0 @@
-package com.sonata;
-
-public class NoLeaveAvailableException extends Exception{
-	//creating an exception when leaves are exhausted
-	public  NoLeaveAvailableException(String e) {
-		super(e);
-	}
-
-}
